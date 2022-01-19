f38646b7f08cf693a237fa8673092d68f31a0c55
Nicolas Orozco Davila <nico.orozco.davila@yandex.com>
2022-01-19 17:38:07 -0500

    Delete unused disabled rule

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Container, Row, Col, ListGroup } from "react-bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
